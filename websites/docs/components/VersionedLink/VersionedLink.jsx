e6d6cb6c2568203fffa9e4b3f7233c1707a10e74
Matthias Wagler <wagler@intuity.de>
2019-09-06 17:08:44 +0200

    Enable more versions to be set inside the VersionedLink component.

--- websites/docs/components/VersionedLink/VersionedLink.jsx
+++ websites/docs/components/VersionedLink/VersionedLink.jsx
@@ -1,16 +1,40 @@
+import get from 'lodash/get';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { usePageContext } from '..';
 
-const VersionedLink = function ({ href, children, rewriteLatestAs } = {}) {
-  const { activePage } = usePageContext();
-  let { version } = activePage;
-
-  if (version === 'latest') {
-    version = rewriteLatestAs;
+const createReplace = function ({ activePage, rewriteLatestAs }) {
+  if (!activePage) {
+    throw new Error('Active page is missing.');
   }
 
-  return <a href={ href.replace('#VERSION#', version) }>{ children }</a>;
+  return function (match, pattern) {
+    switch (pattern) {
+      case 'VERSION': {
+        let { version } = activePage;
+
+        if (version === 'latest') {
+          version = rewriteLatestAs;
+        }
+
+        return version;
+      }
+      default: {
+        const lowerCasePath = pattern.toLowerCase();
+        const version = get(activePage, lowerCasePath);
+
+        return version;
+      }
+    }
+  };
+};
+
+const searchForVersionsRegularExpression = new RegExp('#(?<version>VERSION|VERSIONS.NODE)#', 'gu');
+
+const VersionedLink = function ({ href, children, rewriteLatestAs } = {}) {
+  const { activePage } = usePageContext();
+
+  return <a href={ href.replace(searchForVersionsRegularExpression, createReplace({ activePage, rewriteLatestAs })) }>{ children }</a>;
 };
 
 VersionedLink.propTypes = {
