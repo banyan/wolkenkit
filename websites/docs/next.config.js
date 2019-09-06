d9fc07e2b0165e89dad48b036fb17b3b4bd827a7
Matthias Wagler <wagler@intuity.de>
2019-09-06 19:20:53 +0200

    Generate sitemap.txt on export.

--- websites/docs/next.config.js
+++ websites/docs/next.config.js
@@ -2,8 +2,12 @@
 'use strict';
 /* eslint-enable strict */
 
+const path = require('path');
+
 const processenv = require('processenv'),
       slug = require('remark-slug');
+
+const write = require('../shared/file/write');
 
 const isProduction = processenv('NODE_ENV') === 'production';
 
@@ -16,7 +20,19 @@ const withMDX = require('@next/mdx')({
 
 const config = {
   exportTrailingSlash: isProduction,
-  pageExtensions: [ 'js', 'jsx', 'mdx' ]
+  pageExtensions: [ 'js', 'jsx', 'mdx' ],
+  async exportPathMap (defaultPathMap, { dev, outDir }) {
+    const sitemapFileName = path.join(outDir, 'sitemap.txt');
+
+    if (!dev) {
+      const allPageUrls = Object.keys(defaultPathMap).
+        reduce((urls, relativeUrl) => `${urls}${relativeUrl}\n`, '');
+
+      await write(sitemapFileName, allPageUrls);
+    }
+
+    return defaultPathMap;
+  }
 };
 
 module.exports = withMDX(config);
